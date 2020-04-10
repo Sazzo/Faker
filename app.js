aac41417fe6792a38265e9b1ca05f67ee93f2a06
Sazzo <stolensoul32@gmail.com>
2020-04-10 17:17:36 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+var app = express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.render('pages/index')
+})
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
